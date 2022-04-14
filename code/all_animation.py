e898bfce36c0c749331334020d6c61a259ba4971
a-gucciardi <arnaud.gucciardi@toelt.ai>
2022-04-14 10:26:23 +0200

    animations clean

--- code/all_animation.py
+++ code/all_animation.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-results = pd.read_csv("../files/Object Hit - [Child - practice] - RIGHT - 12_02.csv",encoding= 'unicode_escape', sep=',',skiprows = 390, parse_dates=[0,1,2])
+results = pd.read_csv("../files/Object_Hit_-_[Child_-_practice]_-_RIGHT_-_12_02.csv",encoding= 'unicode_escape', sep=',',skiprows = 390, parse_dates=[0,1,2])
 
 gazeX = results['Gaze_X']
 gazeY = results['Gaze_Y']
@@ -14,7 +14,7 @@ leftX = results['Left: Hand position X']
 leftY = results['Left: Hand position Y']
 #leftSpeed = results['Left: Hand speed']
 time = results['Frame time (s)']
-time = [str(round(i, 4)) for i in time]
+time = [int(round(i, 4)) for i in time]
 
 def plot_animation():
     fig, ax = plt.subplots()
@@ -22,15 +22,15 @@ def plot_animation():
         ax.plot(rightX[i],rightY[i],'ro')
         ax.plot(leftX[i],leftY[i],'bo')
         ax.plot(gazeX[i],gazeY[i],'go')
-        ax.set_title(time[i] + ' ms')
-        #ax.set_title(time[i])
+        if time[i] % 1 == 0:
+            ax.set_title(str(time[i]) + ' ms')
         ax.set_xlim([-0.5,0.5])
         ax.set_ylim([0,1])
 
     anim = FuncAnimation(fig, animate, frames=int(len(gazeX)/4), interval=5, repeat=False)
     plt.show()
-    # save
-    f = r"./animate_func.gif"
+    #save
+    f = r"./animate_func_all.gif"
     #anim.save(f, fps=30)
     plt.close()
 
@@ -39,12 +39,12 @@ def plot_static():
     fig = plt.figure(figsize=(8,5))
     plt.xlim([-0.5,0.5])
     plt.ylim([0,1])
-    i = 0
-    while i < len(gazeX):
-        plt.plot(rightX[i],rightY[i],'ro')
-        plt.plot(leftX[i],leftY[i],'bo')
-        plt.plot(gazeX[i],gazeY[i],'go')
-        i+=1
+
+    plt.plot(rightX,rightY,'ro', label='right')
+    plt.plot(leftX,leftY,'bo', label='left')
+    plt.plot(gazeX,gazeY,'go', label='gaze')
+
+    plt.legend()
     plt.show()
 
 plot_animation()
--- code/eye_animation.py
+++ code/eye_animation.py
@@ -3,36 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-results = pd.read_csv("../files/Object Hit - [Child - practice] - RIGHT - 12_02.csv",encoding= 'unicode_escape', sep=',',skiprows = 390, parse_dates=[0,1,2])
+results = pd.read_csv("../files/Object_Hit_-_[Child_-_practice]_-_RIGHT_-_12_02.csv",encoding= 'unicode_escape', sep=',',skiprows = 390, parse_dates=[0,1,2])
 
 gazeX = results['Gaze_X']
 gazeY = results['Gaze_Y']
 time = results['Frame time (s)']
-time = [str(round(i, 4)) for i in time]
+time = [int(round(i, 4)) for i in time]
 
 def plot_animation():
     fig, ax = plt.subplots()
     def animate(i):
         ax.plot(gazeX[i], gazeY[i], 'ro')
-        ax.set_title(time[i] + ' ms')
-        #ax.set_title(time[i])
+        if time[i]%1 == 0:
+            #ax.set_title(str(time[i]) + ' ms')
+            print(time[i])
         ax.set_xlim([-0.5,0.5])
         ax.set_ylim([0,1])
 
     anim = FuncAnimation(fig, animate, frames=len(gazeX), interval=5, repeat=False)
-    plt.show()
+    #plt.show()
     # save
-    f = r"./animate_func.gif"
-    anim.save(f, fps=30)
+    f = r"./animate_func2.gif"
+    anim.save(f)
     plt.close()
+
 def plot_static():
     fig = plt.figure(figsize=(8,5))
     plt.xlim([-0.5,0.5])
     plt.ylim([0,1])
-    i = 0
-    while i < len(gazeX):
-        plt.plot(gazeX[i],gazeY[i],'ro')
-        i+=1
+    plt.plot(gazeX,gazeY,'ro')
     plt.show()
 
 plot_animation()
