3d779f47bf066a58cf1aa0d850adfbe02168361a
a-gucciardi <arnaud.gucciardi@toelt.ai>
2022-04-20 10:03:00 +0200

    events + plots fix

--- code/csv_load.py
+++ code/csv_load.py
@@ -27,41 +27,50 @@ class Trial:
         self.count = df.iloc[0][4]
         self.duration = df.iloc[-1][10]
 
-        self.events = []
-        self.events_cnt = 0
+        self.events_cnt = Events(df).counts
+        self.saccades = Events(df).saccades
+        self.fixations = Events(df).fixations
+        self.blinks = Events(df).blinks
+        self.events = {'saccades':self.saccades, 'fixations':self.fixations, 'blinks':self.blinks}
 
         self.kinematics = Kinematics(df).values
         #self.plot = self.plots()
 
-    def plots(self,name="fig_default.png"):
+    def plot_movements(self,name="fig_default.png", save=True, show=False):
         fig = plt.figure()
-        floatx = [float(i) for i in self.kinematics['right_x']]
-        floaty = [float(i) for i in self.kinematics['right_y']]
-        plt.plot(floatx, floaty,'ro', label='right')
-        #plt.plot(dataframe['Left: Hand position X'],dataframe['Left: Hand position Y'],'bo', label='left')
-        #plt.plot(dataframe['Gaze_X'],dataframe['Gaze_Y'],'go', label='gaze')
-
+        plt.plot(self.kinematics['right_x'],self.kinematics['right_y'],'ro', label='right')
+        plt.plot(self.kinematics['left_x'],self.kinematics['left_y'],'bo', label='left')
+        plt.plot(self.kinematics['gaze_x'],self.kinematics['gaze_y'],'go', label='gaze')
         plt.legend()
+        plt.title(self.duration)
 
-        plt.savefig(name)
-        #plt.show()
-        #plt.close(fig)
+        if save: plt.savefig(name)
+        if show: plt.show()
+        else: plt.close(fig)
         return fig 
 
-class Event:
-    def __init__(self) -> None:
-        self.frame = 0
-        self.frame_time = 0
-        self.event_name = ""
+class Events:
+    def __init__(self, df) -> None:
+        event_list = list(df[df['Event name'].notna()]['Event name'])
+        self.counts = {}
+        self.counts['saccades'] = event_list.count('Gaze saccade start')
+        self.counts['fixations'] = event_list.count('Gaze fixation start')
+        self.counts['blinks'] = event_list.count('Gaze blink start')
+        #self.counts['other'] = event_list.count('')
+        df_event = df[df['Event name'].notna()]
+        self.saccades =  [tuple(x) for x in df_event.loc[df_event['Event name'] == 'Gaze saccade start'][['Frame #','Event time (s)']].values]
+        self.fixations = [tuple(x) for x in df_event.loc[df_event['Event name'] == 'Gaze fixation start'][['Frame #','Event time (s)']].values]
+        self.blinks =    [tuple(x) for x in df_event.loc[df_event['Event name'] == 'Gaze blink start'][['Frame #','Event time (s)']].values]
 
+        
 class Kinematics:
     def __init__(self, df) -> None:
         self.values = {}
-        self.values['gaze_x'] = list(df['Gaze_X'])
-        self.values['gaze_y'] = list(df['Gaze_Y'])
+        self.values['gaze_x'] = [float(i) for i in df['Gaze_X']]
+        self.values['gaze_y'] = [float(i) for i in df['Gaze_Y']]
         self.values['right_x'] = list(df['Right: Hand position X'])
         self.values['right_y'] = list(df['Right: Hand position Y'])
         self.values['right_spd'] = list(df['Right: Hand speed'])
         self.values['left_x'] = list(df['Left: Hand position X'])
-        self.values['left_y'] = list(df['Left: Hand position X'])
+        self.values['left_y'] = list(df['Left: Hand position Y'])
         self.values['left_spd'] = list(df['Left: Hand speed'])
--- code/load_test.py
+++ code/load_test.py
@@ -11,7 +11,7 @@ circuit = "../files/utf8/Circuit_Exo_-_[b_Circuit1_(youngerchildren)]_-_RIGHT_-_
 reaching = "../files/utf8/Visually_Guided_Reaching_-_Child_v2_(4_target)_-_LEFT_-_11_50.csv"
 
 
-EXERCISE = ball
+EXERCISE = reaching
 
 if EXERCISE[14] == 'B':
     dfs = extract_dataframes(EXERCISE, offset=6)
@@ -22,21 +22,16 @@ elif EXERCISE[14] == 'O' or 'R' :
 
 
 #print(type(trial1.kinematics['right_x'][0]))
-
-
-def plots(dic):
-        fig = plt.figure()
-        #plt.plot(dic['right_x'],dic['right_y'],'ro', label='right')
-        floatx = [float(i) for i in dic['right_x']]
-        floaty = [float(i) for i in dic['right_y']]
-        plt.plot(floatx, floaty,'ro', label='right')
-        #plt.plot(dataframe['Left: Hand position X'],dataframe['Left: Hand position Y'],'bo', label='left')
-        #plt.plot(dataframe['Gaze_X'],dataframe['Gaze_Y'],'go', label='gaze')
-        plt.legend()
-        #plt.savefig("right_test.svg")
-        #plt.close(fig)
-        plt.show()
-        #return fig 
+# def plots(dic):
+#     fig = plt.figure()
+#     plt.plot(dic['right_x'],dic['right_y'],'ro', label='right')
+#     plt.plot(dic['left_x'],dic['left_y'],'bo', label='left')
+#     #plt.plot(dataframe['Gaze_X'],dataframe['Gaze_Y'],'go', label='gaze')
+#     plt.legend()
+#     #plt.savefig("right_test.svg")
+#     #plt.close(fig)
+#     plt.show()
+#     #return fig 
 
 for i in range(len(dfs)):
     print(i)
@@ -44,4 +39,10 @@ for i in range(len(dfs)):
     print(trial.count)
     #plots(trial.kinematics)
     title = str(i) + EXERCISE[14:17] + "right_test.png"
-    trial.plots(title)
+
+    trial.plot_movements(title, save=False, show=True)
+    #plt.show()         #Controls the plotting or not when running it, like plot=True
+    
+    print(trial.events_cnt)
+    print(trial.events)
+
